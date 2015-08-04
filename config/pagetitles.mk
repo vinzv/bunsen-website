87bc51ef5a3b1accc6f9ee0b40aba6303bade8f5
Jens John <dev@2ion.de>
2015-08-04 12:37:16 +0200

    Better page titles for imprint & activity log

--- config/pagetitles.mk
+++ config/pagetitles.mk
@@ -2,4 +2,5 @@ src/index.mkd.title = BunsenLabs
 src/repository.mkd.title = BunsenLabs :: Repository
 src/installation.mkd.title = BunsenLabs :: Installation
 src/resources.mkd.title = BunsenLabs :: Resources
-src/gitlog.mkd.title = BunsenLabs :: Commit Log
+src/gitlog.mkd.title = BunsenLabs :: Activity Log
+src/impressum.mkd.title = BunsenLabs :: Imprint
