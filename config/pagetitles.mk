17bbe6b1275fc110094bd5bf7d9191647d56c7fd
Jens John <dev@2ion.de>
2015-08-06 22:35:44 +0200

    Changes to page titles and descriptions

--- config/pagetitles.mk
+++ config/pagetitles.mk
@@ -5,3 +5,4 @@ src/installation.mkd.title = BunsenLabs :: Installation
 src/resources.mkd.title = BunsenLabs :: Resources
 src/gitlog.mkd.title = BunsenLabs :: Activity Log
 src/imprint.mkd.title = BunsenLabs :: Imprint
+src/legal.mkd.title = BunsenLabs :: Legal Remarks
