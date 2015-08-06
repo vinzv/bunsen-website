0c5deabfc4862ed1492b4e01902288fa22fb312d
Jens John <dev@2ion.de>
2015-08-06 22:30:25 +0200

    legal.mkd: List trademarks used
      * Linux (check with the LF if we need to apply for a sublicense)
      * Debian
      * netcup

--- config/pagetitles.mk
+++ config/pagetitles.mk
@@ -4,4 +4,4 @@ src/repository.mkd.title = BunsenLabs :: Repository
 src/installation.mkd.title = BunsenLabs :: Installation
 src/resources.mkd.title = BunsenLabs :: Resources
 src/gitlog.mkd.title = BunsenLabs :: Activity Log
-src/impressum.mkd.title = BunsenLabs :: Imprint
+src/imprint.mkd.title = BunsenLabs :: Imprint
