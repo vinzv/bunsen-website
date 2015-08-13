aa241064a71e871208de1c82e3fb906db9de74f5
Jens John <dev@2ion.de>
2015-08-13 13:04:51 +0200

    navigation: Add title= attributes to links

--- config/pagedescriptions.mk
+++ config/pagedescriptions.mk
@@ -1,5 +1,5 @@
 # vim: set expandtab
 src/index.mkd.description = BunsenLabs Linux is a light-weight distribution offering a lean but tweakable Openbox desktop.
-src/installation.mkd.description = How to install BunsenLabs Linux
+src/installation.mkd.description = Installation instructions
 src/resources.mkd.description = Development resources and package sources
 src/legal.mkd.description = Legal remarks
