4a91f4dabe9598932983a0fadd7a68f02a77b886
Jens John <dev@2ion.de>
2015-09-12 19:18:46 +0200

    Remove html5shiv for IE <9

--- config/pagedescriptions.mk
+++ config/pagedescriptions.mk
@@ -2,4 +2,4 @@
 src/index.mkd.description = BunsenLabs Linux is a light-weight distribution offering a lean but tweakable Openbox desktop.
 src/installation.mkd.description = Installation instructions
 src/resources.mkd.description = Development resources and package sources
-src/legal.mkd.description = Legal remarks
+src/legal.mkd.description = Legal remarks and attribution
