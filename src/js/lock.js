f8c64d373bb0007ff2ab0d767be02042df680c80
Jens John <dev@2ion.de>
2015-09-12 19:12:23 +0200

    lock.js: Add comments, use absolute paths

--- src/js/lock.js
+++ src/js/lock.js
@@ -1,14 +1,23 @@
-function include_lock() {
-  var node = document.querySelector(".padlock");
+/*
+ * If the user accesses the site using a non-SSL connection, an opened,
+ * red padlock is displayed which upon clicking will redirect the user
+ * to the HTTPS version.
+ */
+
+function include_lock(anchor, image) {
+  var node = document.querySelector(anchor);
   if(node===null) return;
   if(window.location.protocol != 'https:') {
     var a = document.createElement("a");
     a.setAttribute("href", "https://"+window.location.host + window.location.pathname);
     a.setAttribute("title", "Switch to a SSL-secured connection");
     var img = document.createElement("img");
-    img.setAttribute("src", "img/numix_lock.svg");
+    img.setAttribute("src", image);
     a.appendChild(img);
     node.appendChild(a);
   }
 };
-include_lock();
+
+const PADLOCK_IMAGE = "/img/numix_lock.svg";
+const PADLOCK_ANCHOR = ".padlock";
+include_lock(PADLOCK_ANCHOR, PADLOCK_IMAGE);
