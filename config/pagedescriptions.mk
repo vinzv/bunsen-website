fb24a8ddd6a55984bc2a66166a48c4454129528b
Jens John <dev@2ion.de>
2015-09-17 21:36:12 +0200

    index: Correct page description for Google Index

--- config/pagedescriptions.mk
+++ config/pagedescriptions.mk
@@ -1,5 +1,5 @@
 # vim: set expandtab
-src/index.mkd.description = BunsenLabs Linux is a light-weight distribution offering a lean but tweakable Openbox desktop.
+src/index.mkd.description = BunsenLabs Linux is a distribution offering a light-weight and easily customizable Openbox desktop
 src/installation.mkd.description = Installation instructions
 src/resources.mkd.description = Development resources and package sources
 src/legal.mkd.description = Legal remarks and attribution
