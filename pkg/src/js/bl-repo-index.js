e2b7fcf17a006cdc4c04d5ee9f1f8a1c19516429
Jens John <dev@2ion.de>
2016-03-18 23:59:26 +0900

    pkg:js:bl-repo-index:apply GPL

--- pkg/src/js/bl-repo-index.js
+++ pkg/src/js/bl-repo-index.js
@@ -1,9 +1,20 @@
 /*
- * bl-repo-index - produce an index of our APT repo
- * Written by 2ion for the BunsenLabs Project.
- *
- * FYI this is the first JS program I ever wrote. Expect nothing.
- */
+  bl-repo-index -- package index generator for Debian APT repositories
+  Copyright (C) 2015-2016 Jens John <dev@2ion.de>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 'use strict';
 
