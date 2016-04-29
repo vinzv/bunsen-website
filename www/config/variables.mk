08dbe833d432b1a903321cabac55e9680411ca09
Jens John <dev@2ion.de>
2016-04-29 16:55:27 +0900

    www: updates for BL Hydrogen final

--- www/config/variables.mk
+++ www/config/variables.mk
@@ -4,31 +4,37 @@
 
 RELEASE_CODENAME = Hydrogen
 RELEASE_CODENAME_LOWERCASE=$(shell echo $(RELEASE_CODENAME) | tr '[A-Z]' '[a-z]')
-RELEASE_VERSION = rc2
-RELEASE_DATE = February 16, 2016
-RELEASE_ANNOUNCEMENT_URL = https://forums.bunsenlabs.org/viewtopic.php?id=1321
+RELEASE_VERSION =
+RELEASE_DATE = April 29, 2016
+RELEASE_ANNOUNCEMENT_URL = https://forums.bunsenlabs.org/viewtopic.php?id=1797
 
 DDL_BASE_URL = https://kelaino.bunsenlabs.org/ddl
-ISO_BASE_NAME = bl-$(RELEASE_CODENAME)-$(RELEASE_VERSION)
+ISO_BASE_NAME = bl-$(RELEASE_CODENAME)$(RELEASE_VERSION)
 
 ISO_32 = $(ISO_BASE_NAME)-i386.iso
+ISO_32CD = $(ISO_BASE_NAME)-i386+NonPAE.iso
 ISO_64 = $(ISO_BASE_NAME)-amd64.iso
 
-ISO_32_SIZE = 879M
-ISO_64_SIZE = 825M
+ISO_32_SIZE = 960M
+ISO_32CD_SIZE = 659M
+ISO_64_SIZE = 836M
 
 DDL_URL_32 = $(DDL_BASE_URL)/$(ISO_32)
 DDL_URL_32M = https://kelaino.bunsenlabs.org/ddl/$(ISO_32)
+DDL_URL_32CD = $(DDL_BASE_URL)/$(ISO_32CD)
 DDL_URL_64 = $(DDL_BASE_URL)/$(ISO_64)
 DDL_URL_64M = https://kelaino.bunsenlabs.org/ddl/$(ISO_64)
 
 SHA256SUMS_URL_32 = $(DDL_URL_32).sha256sum.txt
+SHA256SUMS_URL_32CD = $(DDL_URL_32CD).sha256sum.txt
 SHA256SUMS_URL_64 = $(DDL_URL_64).sha256sum.txt
 
 SIG_URL_32 = $(DDL_URL_32).sig
+SIG_URL_32CD = $(DDL_URL_32CD).sig
 SIG_URL_64 = $(DDL_URL_64).sig
 
 TORRENT_URL_32 = $(DDL_URL_32).torrent
+TORRENT_URL_32CD = $(DDL_URL_32CD).torrent
 TORRENT_URL_64 = $(DDL_URL_64).torrent
 
 # Make sure to escape '&' in the URL because it's used as input to sed
@@ -45,12 +51,14 @@ GALLERY_NOSCRIPT = $(shell cat src/include/index_gallery_noscript.html|sed 's/^\
 RELEASE_SUBST :=             \
 	DDL_URL_32                 \
 	DDL_URL_32M                \
+	DDL_URL_32CD               \
 	DDL_URL_64                 \
 	DDL_URL_64M                \
 	GALLERY                    \
 	GALLERY_NOSCRIPT           \
-	ISO_32 ISO_64              \
+	ISO_32 ISO_64 ISO_32CD     \
 	ISO_32_SIZE                \
+	ISO_32CD_SIZE              \
 	ISO_64_SIZE                \
 	RELEASE_ANNOUNCEMENT_URL   \
 	RELEASE_CODENAME           \
@@ -58,10 +66,11 @@ RELEASE_SUBST :=             \
 	RELEASE_DATE               \
 	RELEASE_VERSION            \
 	SHA256SUMS_URL_32          \
+	SHA256SUMS_URL_32CD        \
 	SHA256SUMS_URL_64          \
 	SIG_URL_32                 \
+	SIG_URL_32CD               \
 	SIG_URL_64                 \
-	TORRENT_MAGNET_LINK_32     \
-	TORRENT_MAGNET_LINK_64     \
 	TORRENT_URL_32             \
+	TORRENT_URL_32CD           \
 	TORRENT_URL_64
