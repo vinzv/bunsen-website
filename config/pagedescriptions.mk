0627c87380a077acba8f49771d27bf11149cdc99
Jens John <dev@2ion.de>
2016-11-22 20:18:21 +0100

    gallery:update 1 picture

--- config/pagedescriptions.mk
+++ config/pagedescriptions.mk
@@ -1,13 +1,13 @@
 # vim: set expandtab
 src/index.mkd.description = BunsenLabs Linux is a distribution offering a light-weight and easily customizable Openbox desktop
 src/installation.mkd.description = Installation and download instructions
-src/resources.mkd.description = Community and development resources and BL team members
+src/resources.mkd.description = Community/development resources and BL team members
 src/legal.mkd.description = Legal remarks, licenses, attribution
-src/donations.mkd.description = Support BunsenLabs with a donation
-src/donations.intermediate.mkd.description = Support BunsenLabs with a donation
-src/stuffweuse.mkd.description = Technologies used by the BL project
+src/donations.mkd.description = Support BunsenLabs with a direct donation or via affiliate links
+src/donations.intermediate.mkd.description = Support BunsenLabs with a direct donation or via affiliate links
+src/stuffweuse.mkd.description = List of open-source technologies and software used by the project
 src/forums403.mkd.description = About the anti-spam IP block lists employed on forums.bunsenlabs.org
 src/repositories.mkd.description = APT repository configuration, mirrors, signing key and package search
-src/repoidx.mkd.description = BunsenLabs package search
+src/repoidx.mkd.description = BunsenLabs package list and search
 src/imprint.mkd.description = Site imprint and webmaster contact
 src/gitlog.mkd.description = Commit activity of public BunsenLabs software repositories
