1c382821915c7e73b60a49e476834e0e20e15128
Jens John <dev@2ion.de>
2016-12-01 14:38:28 +0100

    config:merge descr and titles

---
+++ config/pages.mk
@@ -0,0 +1,30 @@
+# vim: set expandtab
+
+BASE_PAGE_TITLE                            = BunsenLabs Linux
+
+src/index.mkd.description                  = BunsenLabs Linux is a distribution offering a light-weight and easily customizable Openbox desktop
+src/installation.mkd.description           = Installation and download instructions
+src/resources.mkd.description              = Community/development resources and BL team members
+src/legal.mkd.description                  = Legal remarks, licenses, attribution
+src/donations.mkd.description              = Support BunsenLabs with a direct donation or via affiliate links
+src/donations.intermediate.mkd.description = Support BunsenLabs with a direct donation or via affiliate links
+src/stuffweuse.mkd.description             = List of open-source technologies and software used by the project
+src/forums403.mkd.description              = About the anti-spam IP block lists employed on forums.bunsenlabs.org
+src/repositories.mkd.description           = APT repository configuration, mirrors, signing key and package search
+src/repoidx.mkd.description                = BunsenLabs package list and search
+src/imprint.mkd.description                = Site imprint and webmaster contact
+src/gitlog.mkd.description                 = Commit activity of public BunsenLabs software repositories
+
+src/index.mkd.title                        = $(BASE_PAGE_TITLE)
+src/repository.mkd.title                   = $(BASE_PAGE_TITLE) :: Repository
+src/installation.mkd.title                 = $(BASE_PAGE_TITLE) :: Installation
+src/resources.mkd.title                    = $(BASE_PAGE_TITLE) :: Resources
+src/gitlog.mkd.title                       = $(BASE_PAGE_TITLE) :: Activity Log
+src/imprint.mkd.title                      = $(BASE_PAGE_TITLE) :: Imprint
+src/legal.mkd.title                        = $(BASE_PAGE_TITLE) :: Legal Remarks
+src/donations.mkd.title                    = $(BASE_PAGE_TITLE) :: Donations
+src/donations.intermediate.mkd.title       = $(BASE_PAGE_TITLE) :: Donations
+src/stuffweuse.mkd.title                   = $(BASE_PAGE_TITLE) :: Stuff We Use
+src/forums403.mkd.title                    = $(BASE_PAGE_TITLE) :: Forums 403 FAQ
+src/repositories.mkd.title                 = $(BASE_PAGE_TITLE) :: Repositories
+src/repoidx.mkd.title                      = $(BASE_PAGE_TITLE) :: Repository index
