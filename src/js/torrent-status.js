3c45acacd25dd731f3c572f45867bdb559d69761
Jens John <dev@2ion.de>
2016-12-06 17:14:01 +0100

    js:torrent-status.js:Work around lack of .forEach on NodeList object on Firefox versions < 50

--- src/js/torrent-status.js
+++ src/js/torrent-status.js
@@ -1,3 +1,15 @@
+/* FF >= 50 supports forEach on NodeList though while FF < 50 does not.
+ * This function plays this issue over. Oh jeez. */
+function nodelistwrap(elem) {
+  if(typeof(elem.forEach) == "undefined") {
+    let a = [];
+    for(let i of elem)
+      a.push(i);
+    return a;
+  }
+  return elem;
+}
+
 function update_torrent_status() {
   let r = new XMLHttpRequest();
   r.open("GET", "https://www.bunsenlabs.org/tracker/status");
@@ -6,7 +18,7 @@ function update_torrent_status() {
     switch(r.status) {
       case 200:
 	d = JSON.parse(r.response);
-	document.querySelectorAll(".torrent-status").forEach((n) => {
+	nodelistwrap( document.querySelectorAll(".torrent-status") ).forEach((n) => {
 	  let id = n.getAttribute("id");
 	  if(id in d.torrents) {
 	    n.textContent = `⬆${ d.torrents[id].s } ⬇${ d.torrents[id].l }`;
