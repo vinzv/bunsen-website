d3c66bb384d20f13592beba30e5cd5ff98945399
Jens John <dev@2ion.de>
2017-02-10 21:39:50 +0100

    Publish some new parts

---
+++ src/js/news.js
@@ -0,0 +1,30 @@
+/* FIXME: missing:old entry link */
+"use strict";
+function update_news() {
+  fetch("https://www.bunsenlabs.org/feed/news")
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((json) => {
+      let anchor = document.querySelector(".news");
+      /* A bit too destructive ^^ */
+      while(anchor.firstChild) anchor.removeChild(anchor.firstChild);
+      json.entries.forEach((e) => {
+        let h2 = document.createElement("h2");
+        h2.setAttribute("class", "news-heading");
+        
+        let span = document.createElement("span");
+        span.setAttribute("class", "news-updated");
+        span.textContent = e.date;
+        h2.appendChild(span);
+        
+        let a = document.createElement("a");
+        a.setAttribute("href", e.link);
+        a.textContent = e.title;
+        h2.appendChild(a);
+
+        anchor.appendChild(h2);
+      });
+    });
+};
+update_news();
